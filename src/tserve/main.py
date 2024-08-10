62df42bc50835b936efc1a319e0e0e5fafec2cbc
Hüseyin ABANOZ <huseyinabanozis@gmail.com>
2024-08-10 16:51:02 +0300

    use cuda.

--- src/tserve/main.py
+++ src/tserve/main.py
@@ -6,7 +6,7 @@ app = FastAPI()
 
 model_name = "habanoz/distilbert-base-multilingual-cased-5000-raw-lr2_5-3ep"
 tokenizer = AutoTokenizer.from_pretrained(model_name)
-model = AutoModelForSequenceClassification.from_pretrained(model_name)
+model = AutoModelForSequenceClassification.from_pretrained(model_name).to("cuda")
 
 
 class Inputs(BaseModel):
@@ -15,11 +15,11 @@ class Inputs(BaseModel):
 
 @app.post("/predict/")
 async def predict(inputs: Inputs = Body(...)):
-    tok_out = tokenizer(inputs.items, padding='max_length', truncation=True, return_tensors='pt')
+    tok_out = tokenizer(inputs.items, padding='max_length', truncation=True, return_tensors='pt').to("cuda")
     logits = model(**tok_out).logits
     predictions = logits.argmax(dim=1).tolist()
     return {"predictions": predictions}
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, port=8000)
